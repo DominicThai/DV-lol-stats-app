58356aa10015172b46959f97be02a537197fd9b9
DominicNr2 <dongu21@student.sdu.dk>
2025-10-20 12:29:31 +0200

    removed the template and uploaded the champs csv in a table

--- shared.py
+++ shared.py
@@ -3,4 +3,4 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-df = pd.read_csv(app_dir / "data" / "penguins.csv")
+champ_df = pd.read_csv(app_dir / "data" / "champs.csv")
