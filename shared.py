bda740e3661612d2e4a879806aceef4a828b1449
Dominic <dominic_thai@live.dk>
2025-10-20 15:42:10 +0200

    changed csv stats to to have coherrent patch number

--- shared.py
+++ shared.py
@@ -3,4 +3,4 @@ from pathlib import Path
 import pandas as pd
 
 app_dir = Path(__file__).parent
-champ_df = pd.read_csv(app_dir / "data" / "champs.csv", sep=';')
+champ_df = pd.read_csv(app_dir / "data" / "champs1.csv", sep=';')
