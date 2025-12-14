3d1ea1f717c9f719c372b56c656b2e3db194df73
Dominic <dominic_thai@live.dk>
2025-12-14 15:13:28 +0100

    resized the ai generated image

--- app.py
+++ app.py
@@ -62,7 +62,9 @@ app_ui = ui.page_navbar(
 ),
     # page 7
     ui.nav_panel("AI generated",
-                 output_widget("ai_generated"),),
+                 ui.img(
+                     src="ai_slop.png", style="width: 700px; height: auto;"
+                     ),),
     
     # page 8
     ui.nav_panel(
